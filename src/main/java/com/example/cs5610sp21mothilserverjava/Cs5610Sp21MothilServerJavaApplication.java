4c7f586b0b555eca011d8cc479f9bddb1efa2a3a
Mothil <mo@mothils-MacPro.local>
2021-01-22 19:41:53 -0800

    Created login and register web pages

---
+++ src/main/java/com/example/cs5610sp21mothilserverjava/Cs5610Sp21MothilServerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs5610sp21mothilserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs5610Sp21MothilServerJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs5610Sp21MothilServerJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cs5610sp21mothilserverjava/Cs5610Sp21MothilServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs5610sp21mothilserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs5610Sp21MothilServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
